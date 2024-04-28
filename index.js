723034e59d31df0f9e8615c15905ec9dd14c8f2f
MD-HABIB-ULLA <habibulla1278@gmail.com>
2024-04-28 15:42:46 +0600

    deploy done

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 4000;
 
+require('dotenv').config()
+ console.log(process.env.DB_PASS)
 app.use(cors());
 app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://NaturealCraft:5MWQGg8rpkq4yWba@cluster0.zqymdgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqymdgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
